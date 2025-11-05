486365d51c5bcc4a4ec0c739f11e56f68b8d7874
Natalia Lach <natlach@student.agh.edu.pl>
2025-11-05 12:54:12 +0100

    Pierwsze zmiany na zajęciach

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ def spare_matrix_Abt(m: int, n: int) -> tuple[np.ndarray, np.ndarray] | None:
             - Wektor b (m,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    if type(m)!=int or type(n)!=int:
+        return None
+    t=np.linspace(0,1,m)
+    b=np.cos(4*t)
+    A=np.vander(t, N=n, increasing=True)
+    return (A,b)
 
 
 def square_from_rectan(
@@ -44,7 +49,12 @@ def square_from_rectan(
             - Wektor b_new (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    if not isinstance(A, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    A_new= A.T @ A
+    b_new=A.T @ b 
+    x=np.linalg.solve(A_new, b_new)
+    return A_new, b_new
 
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
@@ -60,4 +70,8 @@ def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
         (float): Wartość normy residuum dla podanych parametrów.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    #if not isinstance(A, np.ndarray) or not isinstance(b, np.ndarray) or not isinstance(x,np.ndarray):
+        #return None - sprawdź to 
+    return np.linalg.norm(A @ x - b)
+#2 do zastanowienia się nad normami
+#2 zmien zapis czasu i pamieci na taki jak ze wzorca
